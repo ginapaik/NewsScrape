6f7e727fa69cf9284cb173fa8c2758c8b2ec3f65
Gina Paik <ginapaik@outlook>
2018-09-15 11:32:06 -0400

    updates

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-var express = require('express'); // Express Server
-var bodyParser = require('body-parser'); // Post Body Request
-var exphbs = require('express-handlebars'); // Templating Engine
+//////////////////////////////////////////////// /* Imports */ //////////////////////////////////////////////////////////
+let express = require('express'); // Express Server
+let bodyParser = require('body-parser'); // Post Body Request
+let exphbs = require('express-handlebars'); // Templating Engine
 // let logger = require("morgan"); // Logger
 var db = require("./models"); // Require all models
 // let cheerio = require('cheerio'; // Web Scrapper
 // let mongoose = require('mongoose'; // MongoDB ORM
 // let db from "./models"; // Require all models
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-var PORT = process.env.PORT || 8080;
-var app = express(); // Initialize Express
+/////////////////////////////////////////////// /* Set Up Variables*/ //////////////////////////////////////////////////////////
 
+let PORT = process.env.PORT || 8080; // Set Default Port for Express and Heroku
+let app = express(); // Initialize Express
+
+/////////////////////////////////////////////// /* Configure middleware */ //////////////////////////////////////////////////////////
+
+// app.use(logger("dev")); // Use morgan logger for logging requests
 app.use(bodyParser.urlencoded({ extended: false })); // Use body-parser for handling form submissions
 app.use(bodyParser.json());
 app.use(express.static("public")); // Serve static content for the app from the "public" directory in the application directory.
@@ -19,8 +24,10 @@ app.use(express.static("public")); // Serve static content for the app from the 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+/////////////////////////////////////////////// /* Configure Routes */ //////////////////////////////////////////////////////////
 require("./controllers/webScrapperController.js")(app);
 
+/////////////////////////////////////////////// /* Execution */ //////////////////////////////////////////////////////////
 
 app.listen(PORT, ()=>{
     console.log(`App listening on PORT ${PORT}`);
